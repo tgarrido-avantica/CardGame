10bd56b82bee33255ef5521552854c545784fdac
Tomás Garrido Sandino <tgarrido@racsa.co.cr>
2016-08-31 10:17:02 -0600

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Tomás Garrido Sandino on 31/8/16.
+//  Copyright © 2016 Avantica Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Tomás Garrido Sandino on 31/8/16.
+//  Copyright © 2016 Avantica Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
